14d2123256be7a9afa1c740e51b7fddb38d1a290
david <david.ruggeri@tagginfo.com>
2016-12-26 14:44:26 +0100

    boost-filesystem update

--- astream/sources/astream_ErrorCode.h
+++ astream/sources/astream_ErrorCode.h
@@ -11,5 +11,5 @@ typedef enum
 	ERROR_ASTREAM_JOB_UPLOAD_FAILED,					// le job n'a pas pu être soumis (problème d'upload)
 	ERROR_ASTREAM_PRINTRANGE_BAD_FORMAT,			// mauvais format pour le printRange
 	ERROR_ASTREAM_GET_JOB_FAILED,						// erreur lors de la récupération des infos sur le job
-	ERROR_ASTREAM_ADD_JOB_TO_PRINTQUEUE_FAILED,	// le job n'a pas pu être ajouté à la print queue
+	ERROR_ASTREAM_ADD_JOB_TO_PRINTQUEUE_FAILED	// le job n'a pas pu être ajouté à la print queue
 };
